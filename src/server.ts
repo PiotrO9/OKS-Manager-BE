650b69e596b16d46c19f9dfc77222c104d714dbe
PiotrO9 <piotr.ostrowski02@wp.pl>
2026-03-22 11:54:41 +0100

    fix(server): set default port to 3001

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ function createApp() {
 
 async function startServer() {
 	const app = createApp();
-	const port = process.env.PORT || 3000;
+	const port = process.env.PORT || 3001;
 	app.listen(port, () => {
 		// eslint-disable-next-line no-console
 		console.log(`Server listening on http://localhost:${port}`);
